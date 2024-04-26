e827fed1c3afd32c6b0835e03c1f627f566d61bd
Yevgaiz <yevgaiz@gmail.com>
2024-04-26 03:34:44 +0300

    hw 11 fixed

--- HW_11/number_of_words_in_sentence.py
+++ HW_11/number_of_words_in_sentence.py
@@ -4,19 +4,31 @@ words_number = []
 current_count = 0
 
 for i in range(len(sentences)):
-    if sentences[i] == " ":
+    if sentences[i] == " " or i == 1:
         current_count += 1
     elif sentences[i] == "." or sentences[i] == "...":
         if i != len(sentences) - 1:
-            current_count += 1
+            # current_count += 1
             words_number.append(current_count)
             current_count = 0
 
 if current_count > 0:
     words_number.append(current_count)
 
-if len(words_number) == 0 and len(sentences) != 0:
-    words_number = [1]
+# if len(words_number) == 0 and len(sentences) != 0:
+#     words_number = [1]
 
+
+# Variant №2
 print(words_number)
 
+sentence_list = sentences.split(".")
+sentence_list.pop()
+
+sentence_words = []
+
+for sentence in sentence_list:
+    words = sentence.split()
+    sentence_words.append(len(words))
+
+print(sentence_words)
