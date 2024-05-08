d42bad19ce666b582b064d87eca36b034db22d6a
Yevgaiz <yevgaiz@gmail.com>
2024-05-08 23:59:18 +0300

    hw 23 have changed

--- HW_23/second_largest_number_in_the_list.py
+++ HW_23/second_largest_number_in_the_list.py
@@ -1,21 +1,20 @@
 def second_largest_number(nums):
     max_num = second_max_num = float('-inf')
-    max_count = 0
+
 
     for num in nums:
         if num > max_num:
             second_max_num = max_num
             max_num = num
-            max_count = 1
         elif num == max_num:
-            max_count += 1
+            continue
         elif num > second_max_num:
             second_max_num = num
 
-    return second_max_num if second_max_num != float('-inf') and max_count != len(nums) else None
+    return second_max_num if second_max_num != float('-inf') else None
 
 
-print(second_largest_number([22]))
+print(second_largest_number([]))
 print(second_largest_number([1, -10]))
-print(second_largest_number([1, 3, 3, 2]))
+print(second_largest_number([1, 3, 3, 2, 2]))
 print(second_largest_number([1, 3, 2, 4, 16, 25, 10, 5, 16.1]))
