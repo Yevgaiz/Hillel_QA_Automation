5836db8f0ab95e5a83bbb0501f2e496a3b0e399f
Yevgaiz <yevgaiz@gmail.com>
2024-05-14 00:23:44 +0300

    hw 26 has been changed

--- HW_26/call_counter_decorator.py
+++ HW_26/call_counter_decorator.py
@@ -1,19 +1,46 @@
 import os
 
 
+# def call_counter(path):
+#     def decorator(func):
+#         def wrapper(*args, **kwargs):
+#             if not os.path.exists(path):
+#                 with open(path, 'w') as f:
+#                     f.write("Function 'add' was called 0 times")
+
+#             with open(path, 'r') as f:
+#                 count = int(f.read().split()[4])
+#
+#             with open(path, 'w') as f:
+#                 count += 1
+#                 f.write(f"Function '{func.__name__}' was called {count} times\n")
+#
+#             return func(*args, **kwargs)
+#
+#         return wrapper
+#
+#     return decorator
+
+
 def call_counter(path):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            if not os.path.exists(path):
-                with open(path, 'w') as f:
-                    f.write("Function 'add' was called 0 times")
 
-            with open(path, 'r') as f:
-                count = int(f.read().split()[4])
+            if os.path.exists(path):
+                with open(path, 'r') as f:
+                    lines = f.readlines()
+                    counts = {}
+                    for line in lines:
+                        func_name, count_str = line.strip().split(" was called ")
+                        counts[func_name[10:-1]] = int(count_str[:-6])
+            else:
+                counts = {}
+
+            counts[func.__name__] = counts.get(func.__name__, 0) + 1
 
             with open(path, 'w') as f:
-                count += 1
-                f.write(f"Function '{func.__name__}' was called {count} times\n")
+                for func_name, count in counts.items():
+                    f.write(f"Function '{func_name}' was called {count} times.\n")
 
             return func(*args, **kwargs)
 
@@ -27,5 +54,11 @@ def add(a, b):
     return a + b
 
 
+@call_counter('data.txt')
+def divide(a, b):
+    return a / b
+
+
+print(divide(12, 2))
 print(add(4, 6))
 print(add(3, 5))
