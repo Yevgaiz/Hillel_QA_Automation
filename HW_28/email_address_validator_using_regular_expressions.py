59d39a14599914da3c63906d6a7ecc7199e75f23
Yevgaiz <yevgaiz@gmail.com>
2024-05-18 06:02:28 +0300

    hw 28 has been changed 3

--- HW_28/email_address_validator_using_regular_expressions.py
+++ HW_28/email_address_validator_using_regular_expressions.py
@@ -2,7 +2,7 @@ import re
 
 
 def validate_email(email):
-    pattern = r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$'
+    pattern = r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,}$'
     return bool(re.match(pattern, email))
 
 
